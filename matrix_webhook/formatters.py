100f5091bf6b79b8199717f64d90386f7a942fc6
Rune Morling <ermo@serpentos.com>
2022-09-22 15:43:34 +0200

    formatters: Tweak UX for neatness
    - Add repo link (useful for our mono-repo setup)
    - Hide compare behind ref link (too noisy)
    - Only show shortlog for each commit (multiline commits broke links)
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- matrix_webhook/formatters.py
+++ matrix_webhook/formatters.py
@@ -34,13 +34,18 @@ def github(data, headers):
     """Pretty-print a github notification."""
     # TODO: Write nice useful formatters. This is only an example.
     if headers["X-GitHub-Event"] == "push":
-        pusher, ref, a, b, c = (
-            data[k] for k in ["pusher", "ref", "after", "before", "compare"]
+        pusher, repository, ref, a, b, c = (
+            data[k] for k in ["pusher", "repository", "ref", "after", "before", "compare"]
         )
         pusher = f"[@{pusher['name']}](https://github.com/{pusher['name']})"
-        data["body"] = f"{pusher} pushed on {ref}: [{b} → {a}]({c}):\n\n"
+        # Since we use monorepos and use an org-wide webhook, let's add repo info too.
+        repo = f"[{repository['full_name']}]({repository['url']})"
+        # The commit shasum hashes are noisy, so just make the ref link to the full compare
+        data["body"] = f"{repo}: {pusher} pushed on [{ref}]({c}):\n\n"
         for commit in data["commits"]:
-            data["body"] += f"- [{commit['message']}]({commit['url']})\n"
+            # We only really need the shortlog of each relevant commit
+            shortlog = commit['message'].strip().split("\n")[0]
+            data["body"] += f"- [{shortlog}]({commit['url']})\n"
     else:
         data["body"] = "notification from github"
     data["digest"] = headers["X-Hub-Signature-256"].replace("sha256=", "")
