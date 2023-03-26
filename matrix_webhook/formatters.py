4bccac2810d10f126a2188a34f43720f7657c0ac
Rune Morling <ermo@serpentos.com>
2023-03-27 00:15:20 +0200

    formatters.py: Don't report private repo events
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- matrix_webhook/formatters.py
+++ matrix_webhook/formatters.py
@@ -33,22 +33,26 @@ def grafana_9x(data, headers):
 def github(data, headers):
     """Pretty-print a github notification."""
     # TODO: Write nice useful formatters. This is only an example.
-    if headers["X-GitHub-Event"] == "push":
-        pusher, repository, ref, a, b, c = (
-            data[k] for k in ["pusher", "repository", "ref", "after", "before", "compare"]
+    repository = data['repository']
+    if repository['private'] and repository['visibility'] == "private":
+        pass
+        # GH webhook will get a 400 return code w/missing body
+    elif headers['X-GitHub-Event'] == "push":
+        pusher, ref, a, b, c = (
+            data[k] for k in ["pusher", "ref", "after", "before", "compare"]
         )
         pusher_url = f"[@{pusher['name']}](https://github.com/{pusher['name']})"
         # Since we use monorepos and use an org-wide webhook, let's add repo info too.
         repo_url = f"[{repository['full_name']}]({repository['html_url']})"
         # The commit shasum hashes are noisy, so just make the ref link to the full compare
-        data["body"] = f"{repo_url}: {pusher_url} pushed on [{ref}]({c}):\n\n"
-        for commit in data["commits"]:
+        data['body'] = f"{repo_url}: {pusher_url} pushed on [{ref}]({c}):\n\n"
+        for commit in data['commits']:
             # We only really need the shortlog of each relevant commit
             shortlog = commit['message'].strip().split("\n")[0]
-            data["body"] += f"- [{shortlog}]({commit['url']})\n"
-    elif headers["X-GitHub-Event"] == "pull_request":
-        action, number, pr, repo = (
-            data[k] for k in ["action", "number", "pull_request", "repository"]
+            data['body'] += f"- [{shortlog}]({commit['url']})\n"
+    elif headers['X-GitHub-Event'] == "pull_request":
+        action, number, pr = (
+            data[k] for k in ["action", "number", "pull_request"]
         )
         # avoid PR spam and wasted CPU cycles
         if action in ["opened", "closed", "reopened", "edited",
@@ -56,23 +60,23 @@ def github(data, headers):
             pr_title = pr['title']
             pr_url = pr['html_url']
             pr_user = pr['user']['login']
-            reponame = repo['full_name']
-            repo_url = repo['html_url']
+            reponame = repository['full_name']
+            repo_url = repository['html_url']
             url_query = "pulls/"
 
             if action == "closed":
                 url_query="pulls/?q=is%3Apr+is%3Aclosed"
 
-            data["body"] = f"PR#{number} [{pr_title}]({pr_url})\n\n"
-            data["body"] += f"{action} by [@{pr_user}](https://github.com/{pr_user}) "
-            data["body"] += f"in [{reponame}]({repo_url}/{url_query})"
+            data['body'] = f"PR#{number} [{pr_title}]({pr_url})\n\n"
+            data['body'] += f"{action} by [@{pr_user}](https://github.com/{pr_user}) "
+            data['body'] += f"in [{reponame}]({repo_url}/{url_query})"
         # endif
         # GH webhook will get a 400 return code w/missing body if the action
         # isn't in the allow-list
     else:
-        event = headers["X-GitHub-Event"]
-        data["body"] = f"unsupported github event: '{event}'"
-    data["digest"] = headers["X-Hub-Signature-256"].replace("sha256=", "")
+        event = headers['X-GitHub-Event']
+        data['body'] = f"unsupported github event: '{event}'"
+    data['digest'] = headers['X-Hub-Signature-256'].replace("sha256=", "")
     return data
 
 
